0a93aa45ea03546720798c7d89ddc6e92b853a0a
Papacu <viniciussadxxx@gmail.com>
2025-06-05 16:04:22 -0300

    alpha

---
+++ alpha.mk
@@ -0,0 +1,57 @@
+$(call inherit-product, vendor/lineage/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/BoardConfigSoong.mk)
+$(call inherit-product, vendor/lineage/config/BoardConfigLineage.mk)
+$(call inherit-product, device/lineage/sepolicy/common/sepolicy.mk)
+-include vendor/lineage/build/core/config.mk
+
+TARGET_NO_KERNEL_OVERRIDE := true
+TARGET_NO_KERNEL_IMAGE := true
+
+PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
+LOCAL_UNINSTALLABLE_MODULE := true
+
+SELINUX_IGNORE_NEVERALLOWS := true
+
+
+override BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
+TARGET_BOOT_ANIMATION_RES := 720
+TARGET_SUPPORTS_BLUR := false
+
+
+# Device config
+TARGET_HAS_UDFPS := true
+TARGET_ENABLE_BLUR := true
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Build config
+
+# TARGET_BUILD_PACKAGE options:
+# 1 - vanilla (default)
+# 2 - microg
+# 3 - gapps
+TARGET_BUILD_PACKAGE := 3
+
+# Launcher
+TARGET_INCLUDE_LAWNCHAIR := false
+
+# (valid only for GAPPS builds)
+TARGET_INCLUDE_PIXEL_LAUNCHER := false
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_CALL_RECORDING := true
+TARGET_INCLUDE_STOCK_ARCORE := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+
+# Debugging
+TARGET_INCLUDE_MATLOG := false
+WITH_ADB_INSECURE := true
+
+# Extras
+TARGET_INCLUDE_RIMUSIC := false
+
+# Maintainer
+ALPHA_BUILD_TYPE := Unofficial
+ALPHA_MAINTAINER := papacu
---
+++ alphadroid_arm64_bfN.mk
@@ -0,0 +1,26 @@
+TARGET_GAPPS_ARCH := arm64
+include build/make/target/product/aosp_arm64.mk
+$(call inherit-product, device/phh/treble/base.mk)
+
+
+$(call inherit-product, device/phh/treble/axion.mk)
+
+PRODUCT_NAME := alphadroid_arm64_bfN
+PRODUCT_DEVICE := tdgsi_arm64_ab
+PRODUCT_BRAND := google
+PRODUCT_SYSTEM_BRAND := google
+PRODUCT_MANUFACTURER := google
+PRODUCT_SYSTEM_MANUFACTURER := google
+
+PRODUCT_MODEL := AlphaDroid Treble
+
+# Overwrite the inherited "emulator" characteristics
+PRODUCT_CHARACTERISTICS := device
+
+PRODUCT_PACKAGES += 
+
+WITH_ADB_INSECURE := true
+
+PRODUCT_EXTRA_VNDK_VERSIONS += 28 29
+
+TARGET_BUILD_PACKAGE := 2
