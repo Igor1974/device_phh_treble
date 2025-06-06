307f754337a462cc70f1259b698c55601f59a8cd
Papacu <viniciussadxxx@gmail.com>
2025-06-06 12:06:19 -0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -7,5 +7,4 @@ PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/alphadroid_arm64_bvN.mk \
 	$(LOCAL_DIR)/alphadroid_arm64_bfN.mk \
 	$(LOCAL_DIR)/derpfest_arm64_bgN.mk \
-	$(LOCAL_DIR)/derpfest_arm64_bN_slim.mk \
 
