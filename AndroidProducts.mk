824df059ceb8a573e8b9cf8333f349e6f655b99d
Papacu <viniciussadxxx@gmail.com>
2025-06-05 23:29:45 -0300

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,11 @@
+PRODUCT_MAKEFILES := \
+	$(LOCAL_DIR)/infinity_arm64_bvN.mk \
+	$(LOCAL_DIR)/infinity_arm64_bgN.mk \
+	$(LOCAL_DIR)/axion_arm64_bgN.mk \
+	$(LOCAL_DIR)/axion_arm64_bvN.mk \
+	$(LOCAL_DIR)/alphadroid_arm64_bgN.mk \
+	$(LOCAL_DIR)/alphadroid_arm64_bvN.mk \
+	$(LOCAL_DIR)/alphadroid_arm64_bfN.mk \
+	$(LOCAL_DIR)/derpfest_arm64_bgN.mk \
+	$(LOCAL_DIR)/derpfest_arm64_bN_slim.mk \
+
