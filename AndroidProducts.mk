5dbcb2f1ffc2656ab51cdb5bb4da6d5d8149b6bf
Papacu <viniciussadxxx@gmail.com>
2025-06-06 22:31:36 -0300

    infinity

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,7 @@
 PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/infinity_arm64_bvN.mk \
 	$(LOCAL_DIR)/infinity_arm64_bgN.mk \
+	$(LOCAL_DIR)/infinity_gsi.mk \
 	$(LOCAL_DIR)/axion_arm64_bgN.mk \
 	$(LOCAL_DIR)/axion_arm64_bvN.mk \
 	$(LOCAL_DIR)/alphadroid_arm64_bgN.mk \
@@ -10,4 +11,5 @@ PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/horizon_arm64_bgN_pico.mk \
 	$(LOCAL_DIR)/horizon_arm64_bgN_core.mk \
 	$(LOCAL_DIR)/horizon_arm64_bvN.mk \
+
 
