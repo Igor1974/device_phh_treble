7211216759d74a3e4b177f901c583e9568e7c5b1
Papacu <viniciussadxxx@gmail.com>
2025-06-15 00:55:12 -0300

    vol

---
+++ voltage.mk
@@ -0,0 +1,21 @@
+$(call inherit-product, vendor/voltage/config/common.mk)
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
+$(call inherit-product, vendor/voltage/config/BoardConfigSoong.mk)
+$(call inherit-product, vendor/voltage/config/BoardConfigVoltage.mk)
+$(call inherit-product, device/voltage/sepolicy/common/sepolicy.mk)
+-include vendor/voltage/build/core/config.mk
+
+TARGET_NO_KERNEL_OVERRIDE := true
+TARGET_NO_KERNEL_IMAGE := true
+
+PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
+LOCAL_UNINSTALLABLE_MODULE := true
+
+SELINUX_IGNORE_NEVERALLOWS := true
+
+override BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
+TARGET_BOOT_ANIMATION_RES := 720
+
+DERPFEST_BETA = false
+DERPFEST_BUILD_TYPE := Official
