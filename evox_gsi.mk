2fc139c4cf5278cf875937a05f83a49d217fa50a
Papacu <viniciussadxxx@gmail.com>
2025-07-30 14:42:21 -0300

    .

--- evox_gsi.mk
+++ evox_gsi.mk
@@ -2,14 +2,13 @@ TARGET_GAPPS_ARCH := arm64
 include build/make/target/product/aosp_arm64.mk
 $(call inherit-product, device/phh/treble/base.mk)
 #include vendor/gapps/arm64/arm64-vendor.mk
-include vendor/gms/gms_mini.mk
-
+include vendor/gms/gms_pico.mk
 
 $(call inherit-product, device/phh/treble/evox.mk)
 #include vendor/pixel-framework/config.mk
 #include vendor/google/pixel/config.mk
 
-PRODUCT_NAME := evox_mini_gsi
+PRODUCT_NAME := evox_gsi
 PRODUCT_DEVICE := tdgsi_arm64_ab
 PRODUCT_BRAND := google
 PRODUCT_SYSTEM_BRAND := google
@@ -25,9 +24,11 @@ PRODUCT_CHARACTERISTICS := device
 
 PRODUCT_PACKAGES +=
 
+PRODUCT_PACKAGES += apns-conf.xml
+
 WITH_ADB_INSECURE := true
 WITH_GMS := true
-TARGET_USES_MINI_GAPPS := true
+TARGET_USES_PICO_GAPPS := true
 
 WITH_SU := false
 EVO_BUILD_TYPE := Unofficial
