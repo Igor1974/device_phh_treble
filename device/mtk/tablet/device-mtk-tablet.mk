dbdf752df461be96d271e8dce9a018b8e4485012
Igor1974 <fedorov.igor777@gmail.com>
2025-08-10 18:04:50 +0300

    MTK Tablet Fix Kit: Universal fix for MT8788-based tablets
    - Add libmedia_codeclist_capabilities.so for 32/64-bit
    - Fix SELinux: init_daemon_domain(sysfs_mm) for /sys/class/mm
    - Fix ubsan: sub-overflow in AudioCapabilities::getDefaultFormat
    - Required for F+ T800, Lenovo TB-87xx, Alldocube iPlay
    
    Files:
    - proprietary-files/mtk/tablet/
    - device/mtk/tablet/device-mtk-tablet.mk
    - sepolicy/mtk_tablet/
    - proprietary-files.txt
    
    Do not use globally. Use only for MediaTek tablets.

---
+++ device/mtk/tablet/device-mtk-tablet.mk
@@ -0,0 +1,24 @@
+# MTK Tablet Fix Kit: Universal fix for MediaTek-based tablets (MT8788, MT8768, MT8167, etc.)
+# Devices: F+ T800, Lenovo TB-87xx, Alldocube iPlay, and others
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Fixes: ubsan: sub-overflow in libmedia_codeclist_capabilities.so
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/../../proprietary/mtk/tablet/lib64/libmedia_codeclist_capabilities.so:system/lib64/libmedia_codeclist_capabilities.so \
+    $(LOCAL_PATH)/../../proprietary/mtk/tablet/lib/libmedia_codeclist_capabilities.so:system/lib/libmedia_codeclist_capabilities.so
+
+# === 2. SELinux: доступ к /sys/class/mm ===
+# Fixes: avc: denied { search } for name="mm"
+BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/../../sepolicy/mtk_tablet
+
+# === 3. Название устройства ===
+PRODUCT_NAME := mtk_tablet
+PRODUCT_DEVICE := mtk_tablet
+PRODUCT_BRAND := MediaTek
+PRODUCT_MODEL := MT87xx Tablet
+PRODUCT_MANUFACTURER := MediaTek
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.setupwizard.mode=OPTIONAL
