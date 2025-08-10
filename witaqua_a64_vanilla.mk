4cb6a0de78f1b024c166985f142852c8f94a5770
Papacu <viniciussadxxx@gmail.com>
2025-08-09 23:35:23 -0300

    witaqua ota

--- witaqua_a64_vanilla.mk
+++ witaqua_a64_vanilla.mk
@@ -1,13 +1,11 @@
-TARGET_GAPPS_ARCH := arm64
+TARGET_GAPPS_ARCH := arm
 include build/make/target/product/aosp_arm64.mk
 $(call inherit-product, device/phh/treble/base.mk)
 
-$(call inherit-product-if-exists, vendor/microg/microg.mk)
-
 $(call inherit-product, device/phh/treble/wit.mk)
 
-PRODUCT_NAME := witaqua_gsi_microg
-PRODUCT_DEVICE := tdgsi_arm64_ab
+PRODUCT_NAME := witaqua_a64_vanilla
+PRODUCT_DEVICE := tdgsi_a64_ab
 PRODUCT_BRAND := google
 PRODUCT_SYSTEM_BRAND := google
 PRODUCT_MANUFACTURER := google
